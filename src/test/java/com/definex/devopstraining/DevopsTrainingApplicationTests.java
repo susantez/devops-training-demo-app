2bf95786ca99ba12f48aa0cf3f191291d76432dc
ertugrulsen <senertugrul91@gmail.com>
2021-06-26 01:48:19 +0300

    added getBookUnitTest

--- src/test/java/com/definex/devopstraining/DevopsTrainingApplicationTests.java
+++ src/test/java/com/definex/devopstraining/DevopsTrainingApplicationTests.java
@@ -1,13 +1,49 @@
 package com.definex.devopstraining;
 
+import com.definex.devopstraining.data.dao.BookRepository;
+import com.definex.devopstraining.data.model.Book;
+import com.definex.devopstraining.service.BookService;
+import com.definex.devopstraining.service.model.BookDto;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DevopsTrainingApplicationTests {
 
+    private MockMvc mockMvc;
+    @Autowired
+    private BookService bookService;
+
+    @MockBean
+    private BookRepository bookRepository;
+
+
+
+
     @Test
-    void contextLoads() {
+    public void getBookTest(){
+
+        Mockito.when(bookRepository.findAll()).thenReturn(Stream
+                .of(new Book(),new Book(),new Book()).collect(Collectors.toList()));
+        List<BookDto> bookList2 = bookService.findAll();
+        assertEquals(3,bookList2.size());
+
     }
+
+
 
 }
